186b50137462ccb64a7d1af7390a3491033dcb9f
GoorezyEST <goorezyestudios@gmail.com>
2023-02-09 15:46:47 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Singletone_PD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
