a02dc99b56841ce98a6f5aeffb71e142086a7012
GoorezyEST <goorezyestudios@gmail.com>
2023-02-09 16:28:10 -0300

    Updated Program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Singleton Design Pattern Example
+
+            // Get the only instance of the MySingleton class using the Instance property
+            MySingleton mySingleton = MySingleton.Instance;
+            mySingleton.DoSomething();
+        }
+    }
+
+    public class MySingleton
+    {
+        // Store the only instance of the MySingleton class
+        private static MySingleton instance = new MySingleton();
+
+        // Make the constructor private so that it can only be called within the class
+        private MySingleton() { }
+
+        // Property to return the only instance of the MySingleton class
+        public static MySingleton Instance
+        {
+            get { return instance; }
+        }
+
+        // Method to show that the singleton class is being used
+        public void DoSomething()
+        {
+            Console.WriteLine("Singleton class code has been called");
+            Console.ReadLine();
         }
     }
 }
